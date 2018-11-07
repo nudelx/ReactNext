ebbf1cd06861033fb4cb60580b965755e9f8a9af
nudelx <nudelx@gmail.com>
2018-11-07 09:46:23 +0200

    done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
       <div className="App">
         <Card title="nirtzur" />
         <Card title="nudelx" />
+        <Card title="orlavy" />
+        <Card title="rond" />
       </div>
     )
   }
